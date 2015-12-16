98c13b3ae56e3069094e943ed5e852a4a5de9ba4
runnerdave <runnerdave@gmail.com>
2015-12-17 06:57:14 +1100

    added log4j to the project

---
+++ Consumer/src/main/java/com/runnerdave/jms/adapter/ConsumerAdapter.java
@@ -0,0 +1,15 @@
+package com.runnerdave.jms.adapter;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConsumerAdapter {
+
+	
+
+	public void sendToMongo(String json) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- Consumer/src/main/java/com/runnerdave/jms/listener/ConsumerListener.java
+++ Consumer/src/main/java/com/runnerdave/jms/listener/ConsumerListener.java
@@ -1,15 +1,44 @@
 package com.runnerdave.jms.listener;
 
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
+import javax.jms.TextMessage;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
+
+import com.runnerdave.jms.adapter.ConsumerAdapter;
 
 @Component
 public class ConsumerListener implements MessageListener {
+	
+	private static Logger logger = LogManager.getLogger(ConsumerListener.class.getName());
+	
+	@Autowired
+	JmsTemplate jmsTemplate;
+	
+	@Autowired
+	ConsumerAdapter consumerAdapter;
 
 	public void onMessage(Message message) {
-		System.out.println("In onMessage()");
+		logger.info("In onMessage()");
+		
+		String json = null;
+		if (message instanceof TextMessage) {
+			try {
+				json = ((TextMessage)message).getText();
+				logger.info("sending json to DB: " + json);
+				consumerAdapter.sendToMongo(json);
+			} catch (JMSException e) {
+				logger.error("message: " + json);
+				jmsTemplate.convertAndSend(json);
+			}
+		}
+		
 	}
 
 }
