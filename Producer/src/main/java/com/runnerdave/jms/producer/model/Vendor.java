8df53f12241637f09706084d6c8c446956c763cd
runnerdave <runnerdave@gmail.com>
2015-12-20 16:35:58 +1100

    Producer baseline

---
+++ Producer/src/main/java/com/runnerdave/jms/producer/model/Vendor.java
@@ -0,0 +1,97 @@
+package com.runnerdave.jms.producer.model;
+
+public class Vendor {
+	private String vendorName;
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+
+	
+
+	@Override
+	public String toString() {
+		return "Vendor [getVendorName()=" + getVendorName() + ", getFirstName()=" + getFirstName() + ", getLastName()="
+				+ getLastName() + ", getAddress()=" + getAddress() + ", getCity()=" + getCity() + ", getState()="
+				+ getState() + ", getZipCode()=" + getZipCode() + ", getPhoneNumber()=" + getPhoneNumber()
+				+ ", getEmail()=" + getEmail() + "]";
+	}
+
+	private String state;
+	private String zipCode;
+	private String phoneNumber;
+	private String email;
+
+	public String getVendorName() {
+		return vendorName;
+	}
+
+	public void setVendorName(String vendorName) {
+		this.vendorName = vendorName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
